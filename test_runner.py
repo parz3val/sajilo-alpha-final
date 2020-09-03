e0d484a02dfec79c329d62f715ea4a0e243a2fb9
Harris Kunwar <iamtheparzival@gmail.com>
2020-09-04 05:23:11 +0545

    Runs test automatically and gives results if the test pass

---
+++ test_runner.py
@@ -0,0 +1,33 @@
+import os
+import platform
+
+
+def get_test_files():
+    test_file_names = []
+    for file in os.listdir("tests"):
+        if file.endswith('.sajilo'):
+            if platform.system() == "Windows":
+                test_file_names.append("tests\\"+str(file))
+            else:
+                test_file_names.append("tests/" + str(file))
+
+    return test_file_names
+
+
+def mac_commands(python_version):
+    commands = []
+    for test_file_name in get_test_files():
+        command = python_version + " sajilo.py " + test_file_name
+        commands.append(command)
+    return commands
+
+
+# Assuming that only I have two versions of python
+if platform.system() == "Darwin:":
+    py = "python3"
+else:
+    py = "python"
+
+
+for term_c in mac_commands(py):
+    os.system(term_c)
